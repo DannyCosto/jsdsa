05145e73c0932d73afd530543c10d28e93803426
Danny <danny.costelloe@gmail.com>
2021-11-26 13:35:18 -0500

    addUpTo

---
+++ addupto.js
@@ -0,0 +1,12 @@
+//example write a function that calculates the sum of all numbers from 1 up to (and including) some number n.
+
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+console.log(addUpTo(6));
+//6+5+4+3+2+1=21
