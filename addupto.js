321435cbe03b78c621df7427e35c3edefff86562
Danny <danny.costelloe@gmail.com>
2021-11-27 10:59:02 -0500

    adduptoV2

--- addupto.js
+++ addupto.js
@@ -7,6 +7,15 @@ function addUpTo(n) {
   }
   return total;
 }
+let t1 = performance.now();
+addUpTo(10);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
+/*
+function addUpTo(){
+  return n * (n+1) / 2;
+}
+*/
 
 console.log(addUpTo(6));
 //6+5+4+3+2+1=21
